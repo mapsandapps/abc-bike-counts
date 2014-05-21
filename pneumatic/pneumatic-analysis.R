5a0d8168f955a961cfdf6c8729b910c6c2914cb9
Mollie Taylor <mollie.taylor@gmail.com>
2014-05-21 12:55:25 -0400

    Adds pneumatic analysis of time of day

--- pneumatic/pneumatic-analysis.R
+++ pneumatic/pneumatic-analysis.R
@@ -6,8 +6,14 @@ pneu <- read.csv("pneumatic-clean.csv",
 pneu$day <- weekdays(as.Date(pneu$date))
 
 # define weekday
-pneu$weekday <- ifelse(pneu$day == "Saturday", "Weekend", ifelse(pneu$day == "Sunday", 
-  "Weekend", "Weekday"))
+pneu$weekday <- ifelse(pneu$day == "Saturday" | pneu$day == "Sunday", 
+  "Weekend", 
+  "Weekday")
+
+# define beltline
+pneu$beltline <- ifelse(substr(pneu$location, 1, 8) == "BeltLine", 
+  "Beltline",
+  "Not Beltline")
 
 
 library(plyr)
@@ -21,38 +27,36 @@ timeOfDayLoc <- ddply(pneu, .(hour, location), summarize,
     sum(sb, na.rm = TRUE) + 
     sum(wb, na.rm = TRUE) + 
     sum(eb, na.rm = TRUE))
-dayTime <- ddply(pneu, .(hour, location, weekday), summarize,
+dayTime <- ddply(pneu, .(hour, location, weekday, beltline), summarize,
   riders = sum(nb, na.rm = TRUE) + 
     sum(sb, na.rm = TRUE) + 
     sum(wb, na.rm = TRUE) + 
     sum(eb, na.rm = TRUE))
+beltlineData <- ddply(pneu, .(hour, weekday, beltline), summarize,
+  ridersPerHour = (sum(nb, na.rm = TRUE) + 
+    sum(sb, na.rm = TRUE) + 
+    sum(wb, na.rm = TRUE) + 
+    sum(eb, na.rm = TRUE)) / length(hour))
 
-library(ggplot2)
-# ggplot(data = timeOfDayLoc,
-#   aes(x = hour, y = riders, fill = location)) +
-# geom_bar(stat = "identity",
-#   position = position_dodge())
+library(reshape)
+beltlineWide <- cast(beltlineData[beltlineData$beltline == "Beltline",], hour ~ weekday)
+notBeltlineWide <- cast(beltlineData[beltlineData$beltline == "Not Beltline",], hour ~ weekday)
+
+allWide <- merge(beltlineWide, notBeltlineWide,
+  by.x = "hour",
+  by.y = "hour",
+  suffixes = c('Beltline', 'Other'))
+
+# round
+allWide[1:length(allWide$hour),2:5] <- round(allWide[1:length(allWide$hour),2:5],
+  3)
 
 
 
-# # plot beltline only
-# ggplot(data = timeOfDayLoc[substr(timeOfDayLoc$location, 1, 8) == "BeltLine",],
-#   aes(x = as.numeric(hour) - 0.5, y = riders)) +
-# geom_bar(stat = "identity") +
-# scale_x_continuous(breaks = seq(0, 24, 4),
-#   limits = c(0, 24))
-
-# # plot non-beltline only
-# ggplot(data = timeOfDayLoc[substr(timeOfDayLoc$location, 1, 8) != "BeltLine",],
-#   aes(x = as.numeric(hour) - 0.5, y = riders)) +
-# geom_bar(stat = "identity") +
-# scale_x_continuous(breaks = seq(0, 24, 4),
-#   limits = c(0, 24))
-
-write.table(dayTime,
-  file = "time-weekday.csv",
-  quote = FALSE,
-  sep = ",",
-  row.names = FALSE)
+# write.table(allWide,
+#   file = "for-d3.csv",
+#   quote = FALSE,
+#   sep = ",",
+#   row.names = FALSE)
 
 
---
+++ pneumatic/time-of-day-multi.js
@@ -0,0 +1,157 @@
+d3.csv("http://mollietaylor.com/abc-bike-counts/pneumatic/for-d3.csv", function(d) {
+  return {
+    hour: d.hour,
+    WeekdayBeltline: +d.WeekdayBeltline,
+    WeekendBeltline: +d.WeekendBeltline,
+    WeekdayOther: +d.WeekdayOther,
+    WeekendOther: +d.WeekendOther
+  };
+}, function(error, rows) {
+  var hour = _.pluck(rows, 'hour');
+  hour.unshift('Hour');
+  var weekdayBeltline = _.pluck(rows, 'WeekdayBeltline');
+  weekdayBeltline.unshift('Weekday Beltline');
+  var weekendBeltline = _.pluck(rows, 'WeekendBeltline');
+  weekendBeltline.unshift('Weekend Beltline');
+  var weekdayOther = _.pluck(rows, 'WeekdayOther');
+  weekdayOther.unshift('Weekday Other');
+  var weekendOther = _.pluck(rows, 'WeekendOther');
+  weekendOther.unshift('Weekend Other');
+  console.log(weekendOther);
+  // build viz here:
+  var chart1 = c3.generate({
+    bindto: '#weekday-beltline-chart',
+    data: {
+      x: 'Hour',
+      x_format: '%H:%M',
+      columns: [
+        hour,
+        weekdayBeltline
+      ],
+      type: 'bar'
+    },
+    legend: {
+      show: false
+    },
+    axis: {
+      y: {
+        label: 'Riders per Hour'
+      },
+      x: {
+        type: 'timeseries',
+        tick: {
+          format: '%I:%M %p'
+        }
+      }
+    },
+    bar: {
+      width: {
+        ratio: 0.8
+      }
+    }
+  });
+
+  var chart2 = c3.generate({
+    bindto: '#weekend-beltline-chart',
+    data: {
+      x: 'Hour',
+      x_format: '%H:%M',
+      columns: [
+        hour,
+        weekendBeltline
+      ],
+      type: 'bar'
+    },
+    legend: {
+      show: false
+    },
+    axis: {
+      y: {
+        label: 'Riders per Hour'
+      },
+      x: {
+        type: 'timeseries',
+        tick: {
+          format: '%I:%M %p'
+        }
+      }
+    },
+    bar: {
+      width: {
+        ratio: 0.8
+      }
+    }
+  });
+
+  var chart3 = c3.generate({
+    bindto: '#weekday-other-chart',
+    data: {
+      x: 'Hour',
+      x_format: '%H:%M',
+      columns: [
+        hour,
+        weekdayOther
+      ],
+      type: 'bar'
+    },
+    legend: {
+      show: false
+    },
+    axis: {
+      y: {
+        label: 'Riders per Hour'
+      },
+      x: {
+        type: 'timeseries',
+        tick: {
+          format: '%I:%M %p'
+        }
+      }
+    },
+    bar: {
+      width: {
+        ratio: 0.8
+      }
+    }
+  });
+
+  var chart4 = c3.generate({
+    bindto: '#weekend-other-chart',
+    data: {
+      x: 'Hour',
+      x_format: '%H:%M',
+      columns: [
+        hour,
+        weekendOther
+      ],
+      type: 'bar'
+    },
+    legend: {
+      show: false
+    },
+    axis: {
+      y: {
+        label: 'Riders per Hour'
+      },
+      x: {
+        type: 'timeseries',
+        tick: {
+          format: '%I:%M %p'
+        }
+      }
+    },
+    bar: {
+      width: {
+        ratio: 0.8
+      }
+    }
+  });
+
+  // setTimeout(function () {
+  //     chart1.axis.max(100);
+  //     chart2.axis.max(100);
+  //     chart3.axis.max(100);
+  //     chart4.axis.max(100);
+  // }, 5000);
+});
+
